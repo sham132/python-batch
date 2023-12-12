ff8d57f63a86ecbf4571f055f88773993c1985fa
HashimThePassionate <hashiimtahir@gmail.com>
2023-12-12 19:35:24 +0500

    adding new changes

---
+++ footer.php
@@ -0,0 +1,20 @@
+<article class="all-browsers">
+  <h1>Most Popular Browsers</h1>
+  <article class="browser">
+    <h2>Google Chrome</h2>
+    <p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+  </article>
+ 
+
+</article>
+
+
+<footer>
+  <p>Author: John Wrick<br>
+  <a href="mailto:hege@gmail.com">Azad Chai Wala</a></p>
+</footer>
+
+</body>
+
+
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="header">
+  <a href="index.php" class="logo"><img style="border-radius:50px; margin-left:100px" width="50px" src="download.png" /></a>
+  <div class="header-right">
+    <a class="active" href="index.php">Home</a>
+    <a href="contact.php">Contact</a>
+    <a href="about.php">About</a>
+	
+	   <a href="Blog.php">Blog</a>
+	   
+	   <a href="Map.php">MAP</a>
+	   
+  </div>
+</div>
