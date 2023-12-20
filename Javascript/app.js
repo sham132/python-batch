67e8a2a6b0e76373e920f614f4589e464f753f90
HashimThePassionate <hashiimtahir@gmail.com>
2023-12-20 20:10:01 +0500

    adding js folder and outline

---
+++ Javascript/app.js
@@ -0,0 +1,32 @@
+
+
+
+
+//Check from the given integer, whether it is positive or negative.
+let num = "11";
+
+if (num % 2 === 0) {
+    console.log("number is even")
+} else if (num % 2 === 1) {
+    console.log("number is odd")
+}
+else {
+    console.log("number is unknown")
+}
+
+
+// Check from the given integer, whether it is positive or negative.
+
+let arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arrayList.forEach(element => {
+
+    if (element % 2 == 0) {
+        console.log(`${element} number is even`)
+
+    }
+    else {
+        console.log(`${element} number is odd`)
+    }
+
+});
