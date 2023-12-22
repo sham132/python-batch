18a431f745cb48bf2be7018b148765e8120aace7
HashimThePassionate <hashiimtahir@gmail.com>
2023-12-22 18:14:54 +0500

    adding new folder and modified files

---
+++ Javascript/Hooks.js
@@ -0,0 +1,63 @@
+
+
+
+// module.exports = function pakTime(name) {
+//     if (name.toLowerCase() == "pak" || name.toLowerCase() == "pakistan" || name.toLowerCase() == "pk")
+//         return new Date();
+//     else
+//         return "not specified";
+// }
+
+
+
+
+// function add(a,b){
+//     return a+b ;
+// }
+// console.log(add(1,2))
+
+
+
+
+// const add1 = (a,b) =>{
+//     return a+b ;
+// }
+
+// console.log(add1(1,2))
+
+
+const add = (a,b) =>
+{
+ return a+b;
+}
+const subtract = (a,b) =>
+{
+ return a-b;
+}
+const multiply = (a,b) =>
+{
+ return a*b;
+}
+const divide = (a=1,b=3) =>   // arrow func ¬ having default values
+{
+ return a/b;
+}
+console.log(add(1,3));
+console.log(subtract(1,3));
+console.log(multiply(1,3));
+console.log(divide()); // call function which have the default values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Javascript/app.js
+++ Javascript/app.js
@@ -3,30 +3,39 @@
 
 
 //Check from the given integer, whether it is positive or negative.
-let num = "11";
+// let num = "11";
 
-if (num % 2 === 0) {
-    console.log("number is even")
-} else if (num % 2 === 1) {
-    console.log("number is odd")
-}
-else {
-    console.log("number is unknown")
-}
+// if (num % 2 === 0) {
+//     console.log("number is even")
+// } else if (num % 2 === 1) {
+//     console.log("number is odd")
+// }
+// else {
+//     console.log("number is unknown")
+// }
 
 
 // Check from the given integer, whether it is positive or negative.
 
-let arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-arrayList.forEach(element => {
+// arrayList.forEach(element => {
 
-    if (element % 2 == 0) {
-        console.log(`${element} number is even`)
+//     if (element % 2 == 0) {
+//         console.log(`${element} number is even`)
 
-    }
-    else {
-        console.log(`${element} number is odd`)
-    }
+//     }
+//     else {
+//         console.log(`${element} number is odd`)
+//     }
 
-});
+// });
+
+
+
+
+const request =require('./Hooks.js');
+
+let name ="pak";
+
+console.log(pakTime(name.toLocaleLowerCase()))
